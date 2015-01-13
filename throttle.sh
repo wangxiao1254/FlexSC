2ba3cfecda03cb62e8a56c5b0cf38eeb64da0a46
Xiao Wang <wangxiao1254@gmail.com>
2015-01-13 17:05:40 +0800

    .

--- throttle.sh
+++ throttle.sh
@@ -1,2 +1,3 @@
-tc qdisc add dev eth1 root handle 1:0 tbf rate 200kbit buffer 1600 limit 3000
-tc qdisc add dev eth1 parent 1:0 handle 10: netem delay 400ms loss 0.03%
+tc qdisc del dev $1 root
+tc qdisc add dev $1 root handle 1:0 tbf rate $2kbit buffer 1600 limit 3000
+tc qdisc add dev $1 parent 1:0 handle 10: netem delay 100ms loss 0%
