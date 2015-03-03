e752d2ac909fb2dd73e2baf661cbcc459d4c1f0d
kartik <kartik@cs.umd.edu>
2015-03-02 20:05:37 -0500

    Adding BadLabelException

---
+++ src/gc/BadLabelException.java
@@ -0,0 +1,11 @@
+package gc;
+
+public class BadLabelException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public BadLabelException(String message) {
+		super(message);
+	}
+
+}
