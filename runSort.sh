b2a2fb0327a6d737afb126690a56e0ec07851194
Xiao Wang <wangxiao1254@gmail.com>
2015-04-30 17:23:02 -0400

    .

---
+++ runSort.sh
@@ -0,0 +1,3 @@
+java -cp bin:lib/* util.GenRunnable example.Sort &
+
+java -cp bin:lib/* util.EvaRunnable example.Sort
--- src/example/Sort.java
+++ src/example/Sort.java
@@ -1,7 +1,5 @@
 package example;
 
-import java.util.Arrays;
-
 import util.EvaRunnable;
 import util.GenRunnable;
 import util.Utils;
@@ -9,54 +7,67 @@ import circuits.BitonicSortLib;
 import flexsc.CompEnv;
 
 public class Sort {
-	static public<T> void compute(CompEnv<T> gen, T[][] inputB){
+	static public<T> T[][] compute(CompEnv<T> gen, T[][] inputA, T[][] inputB){
+		T[][] in = gen.newTArray(inputA.length+inputB.length, 0);
+		System.arraycopy(inputA, 0, in, 0, inputA.length);
+		System.arraycopy(inputB, 0, in,inputA.length, inputB.length);
 		BitonicSortLib<T> lib = new  BitonicSortLib<T>(gen); 
-		lib.sort(inputB, lib.SIGNAL_ONE);
+		lib.sort(in, lib.SIGNAL_ONE);
+		return in;
 	}
 	
 	public static class Generator<T> extends GenRunnable<T> {
 		T[][] inputB;
+		T[][] inputA;
+		T[][] in;
 
 		@Override
 		public void prepareInput(CompEnv<T> gen) {
-			inputB = gen.newTArray(4000, 0);
-			T[] scTemp = gen.inputOfBob(new boolean[4000*16]);
-			for(int i = 0; i < inputB.length; ++i)
-				inputB[i] = Arrays.copyOfRange(scTemp, i*16, i*16+16);
+			inputB = gen.newTArray(10, 0);
+			for(int i = 0; i < 10; ++i)
+				inputB[i] = gen.inputOfBob(Utils.fromInt(i, 32));
+			inputA = gen.newTArray(10, 0);
+			for(int i = 0; i < 10; ++i)
+				inputA[i] = gen.inputOfAlice(Utils.fromInt(i, 32));
 		}
 		
 		@Override
 		public void secureCompute(CompEnv<T> gen) {
-			compute(gen, inputB);
+			in = compute(gen, inputA, inputB);
 		}
 		@Override
 		public void prepareOutput(CompEnv<T> gen) {
+			for(int i = 0; i < 20; ++i)
+				System.out.println(Utils.toInt(gen.outputToAlice(in[i])));
 		}
 	}
 	
 	public static class Evaluator<T> extends EvaRunnable<T> {
 		T[][] inputB;
 		T[] scResult;
+		T[][] inputA;
+		T[][] in;
 		
 		@Override
 		public void prepareInput(CompEnv<T> gen) {
-			inputB = gen.newTArray(4000, 0);
-			boolean[] temp = new boolean[4000*16];
-			for(int i = 0; i < 4000; ++i)
-				System.arraycopy(Utils.fromInt(CompEnv.rnd.nextInt(), 16), 0, temp, 16*i, 16);
+			inputB = gen.newTArray(10, 0);
+			for(int i = 0; i < 10; ++i)
+				inputB[i] = gen.inputOfBob(Utils.fromInt(i, 32));
 			
-			T[] scTemp = gen.inputOfBob(temp);
-			for(int i = 0; i < inputB.length; ++i)
-				inputB[i] = Arrays.copyOfRange(scTemp, i*16, i*16+16);
+			inputA = gen.newTArray(10, 0);
+			inputA = gen.inputOfAlice(new boolean[10][32]);
 		}
 		
 		@Override
 		public void secureCompute(CompEnv<T> gen) {
-			compute(gen, inputB);
+			in = compute(gen, inputA, inputB);
 		}
 		
 		@Override
 		public void prepareOutput(CompEnv<T> gen) {
+			for(int i = 0; i < 20; ++i)
+				gen.outputToAlice(in[i]);
 		}
+		
 	}
 }
