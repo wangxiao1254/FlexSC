7da444a8751b465fd34618d53c5c8c1780cc9be7
Weikeng Chen <w.k@berkeley.edu>
2018-03-20 11:45:05 -0700

    Make the step the same with RecServer, add sleep

--- test/oram/TestCircuitOramRecClient.java
+++ test/oram/TestCircuitOramRecClient.java
@@ -1,17 +1,20 @@
 package oram;
 
+import java.util.concurrent.TimeUnit;
 import oram.TestCircuitOramRec.EvaRunnable;
 import flexsc.Flag;
 
 public class TestCircuitOramRecClient {
 
 	public  static void main(String args[]) throws Exception {
-		for(int i = 8; i <=24 ; i+=4) {
+		for(int i = 12; i <=20 ; i+=2) {
 			Flag.sw.flush();
 			EvaRunnable eva = new EvaRunnable("localhost", 12345);
 			eva.run();
 			Flag.sw.print();
 			System.out.print("\n");
+			
+			TimeUnit.SECONDS.sleep(1);
 		}
 	}
 }
